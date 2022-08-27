f1479e7cdd28ad36e4c033a664296f4f26b72e7a
nayeem miah <nayeemislam5031@gmail.com>
2022-08-27 14:49:36 +0600

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import { useEffect, useState } from 'react';
+
+
+
 function App() {
+  // useing useState
+  const [likeColor, setLikeColor] = useState("");
+  const [users, setUsers] = useState([]);
+  const [singleuser, setSingleuser] = useState({});
+  const [randomUser, setRandomUser] = useState({});
+
+  // USING USEFFCT FOR FETCH
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users/')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+
+
+    //singleuser
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+      .then((res) => res.json())
+      .then((data) => setSingleuser(data));
+    
+    //randomUser
+    fetch("https://randomuser.me/api/")
+      .then((res) => res.json())
+      .then((data) => setRandomUser(data.results[0]));
+
+  },[])
+
+
+  const handleLike =()=> {
+    const color = likeColor ? '' : 'primary';
+    setLikeColor(color);
+  }
+
+  // declaring useState value in single 
+  const {name} = singleuser;
+  const { gender} = randomUser;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Module 40 advande intermideteBonus</h1>
+      <ThumbUpIcon onClick={handleLike} color={likeColor}></ThumbUpIcon>
+
+      <h2>Name: {name}</h2>
+      random user
+
+      <h3>Gender: {gender}</h3>      
+      <h3>Name: {randomUser.name?.first } {randomUser.name?.last} </h3>
+      
+
+      {/* USING MAP FOR DECLAR ALL STATE VALUE  */}
+
+      {users.map((user) => (
+        <li>{user.name}</li>
+      ))}
     </div>
   );
 }
+
 
 export default App;
